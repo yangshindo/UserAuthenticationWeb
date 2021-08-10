64e7a18267c942b247346a15dc2f037ffeebc442
João Mário <64316250+yangshindo@users.noreply.github.com>
2021-08-10 10:37:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -115,8 +115,6 @@ app.get("/submit", function(req, res){
 app.post("/submit", function(req, res){
   const submittedPost = req.body.post;
 
-//Once the user is authenticated and their session gets saved, their user details are saved to req.user.
-  // console.log(req.user.id);
 
   User.findById(req.user.id, function(err, foundUser){
     if (err) {
